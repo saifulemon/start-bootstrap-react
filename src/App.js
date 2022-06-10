f547b00765f32c99d8bbdda6b9e240b25f112cd1
saifulemon <463saiful0@gmail.com>
2022-06-10 21:04:17 +0600

    remove all file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Home } from './Home';
-import { About } from './About';
-import { Contact } from './Contact';
-import { NoMatch } from './NoMatch';
-import { Layout } from './components/Layout'
-import { NavigationBar } from './components/NavigationBar'
+import React from 'react';
 
-
-
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <NavigationBar />
-        <Layout>
-          <Router>
-            <Routes>
-              <Route exact path="/" element={<Home/>} />
-              <Route exact path="/about" element={<About/>} />
-              <Route exact path="/contact" element={<Contact/>} />
-              <Route element={<NoMatch/>} />
-            </Routes>
-          </Router>
-        </Layout>
-      </React.Fragment>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
 
 export default App;
--- src/Contact.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const Contact = () => (
-    <div>
-        <h2>Contact React-Bootstrap</h2>
-        <p>Dolor sit amet consectetur adipisicing elit. Quidem, ratione placeat, eum vero itaque porro sapiente sunt libero et adipisci odio totam sed error, quo consectetur! Molestias, ipsa ab. Minus quisquam nemo ab quia quaerat earum labore obcaecati, numquam, quibusdam repellendus ratione placeat ea ut quod sapiente nobis nulla. Corporis magnam sit ad fugiat perspiciatis natus dolores, hic obcaecati quibusdam est nostrum, quidem doloremque, et dignissimos officiis! Recusandae quae dicta dignissimos corporis minus iure, est unde laudantium iusto rerum ea, eum earum adipisci nam dolore veritatis praesentium accusantium esse eaque.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, consectetur hic atque, maxime soluta ipsa numquam praesentium beatae quo alias deserunt ea aperiam possimus adipisci suscipit. Sed quia repudiandae velit?</p>
-    </div>
-)
